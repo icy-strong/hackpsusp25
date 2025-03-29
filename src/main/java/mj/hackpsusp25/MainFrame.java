73f750237b7650e0a91cd47d0013183dec429cbc
Mark-Yazemboski <PSSAChamp@gmail.com>
2025-03-29 19:44:01 -0400

    Changing Pannels works

--- src/main/java/mj/hackpsusp25/MainFrame.java
+++ src/main/java/mj/hackpsusp25/MainFrame.java
@@ -49,23 +49,47 @@ public class MainFrame extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         buttonGroup1 = new javax.swing.ButtonGroup();
         buttonGroup2 = new javax.swing.ButtonGroup();
-        jPanel1 = new javax.swing.JPanel();
+        jCheckBoxMenuItem1 = new javax.swing.JCheckBoxMenuItem();
+        jCheckBoxMenuItem2 = new javax.swing.JCheckBoxMenuItem();
+        jMenu1 = new javax.swing.JMenu();
+        Main_Pannel = new javax.swing.JPanel();
         jLabel2 = new javax.swing.JLabel();
         But_Org = new javax.swing.JToggleButton();
         But_Inventory = new javax.swing.JToggleButton();
-        jPanel2 = new javax.swing.JPanel();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
         But_Add = new javax.swing.JToggleButton();
         But_Sub = new javax.swing.JToggleButton();
+        Base_Pannel = new javax.swing.JPanel();
+        Inventory_Pannel = new javax.swing.JPanel();
+        jLabel3 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        jLabel4 = new javax.swing.JLabel();
+        jComboBox1 = new javax.swing.JComboBox<>();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+        Add_Pannel = new javax.swing.JPanel();
+        jLabel5 = new javax.swing.JLabel();
+        jTextField2 = new javax.swing.JTextField();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTable2 = new javax.swing.JTable();
+        Sub_Pannel = new javax.swing.JPanel();
+        Org_Pannel = new javax.swing.JPanel();
 
         jLabel1.setText("jLabel1");
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        jCheckBoxMenuItem1.setSelected(true);
+        jCheckBoxMenuItem1.setText("jCheckBoxMenuItem1");
 
-        jPanel1.setBackground(new java.awt.Color(237, 235, 234));
-        jPanel1.setForeground(new java.awt.Color(220, 220, 220));
-        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+        jCheckBoxMenuItem2.setSelected(true);
+        jCheckBoxMenuItem2.setText("jCheckBoxMenuItem2");
+
+        jMenu1.setText("jMenu1");
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setPreferredSize(new java.awt.Dimension(1000, 800));
+
+        Main_Pannel.setBackground(new java.awt.Color(237, 235, 234));
+        Main_Pannel.setForeground(new java.awt.Color(220, 220, 220));
+        Main_Pannel.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jLabel2.setBackground(new java.awt.Color(204, 255, 255));
         jLabel2.setFont(new java.awt.Font("SansSerif", 1, 36)); // NOI18N
@@ -73,7 +97,7 @@ public class MainFrame extends javax.swing.JFrame {
         jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
         jLabel2.setText("AT HOME INVENTORY MANAGMENT SYSTEM");
         jLabel2.setVerticalAlignment(javax.swing.SwingConstants.TOP);
-        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 20, 1180, 90));
+        Main_Pannel.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 20, 1180, 90));
 
         buttonGroup1.add(But_Org);
         But_Org.setFont(new java.awt.Font("SansSerif", 1, 18)); // NOI18N
@@ -84,7 +108,7 @@ public class MainFrame extends javax.swing.JFrame {
                 But_OrgActionPerformed(evt);
             }
         });
-        jPanel1.add(But_Org, new org.netbeans.lib.awtextra.AbsoluteConstraints(590, 140, 150, 60));
+        Main_Pannel.add(But_Org, new org.netbeans.lib.awtextra.AbsoluteConstraints(590, 140, 150, 60));
 
         buttonGroup1.add(But_Inventory);
         But_Inventory.setFont(new java.awt.Font("SansSerif", 1, 18)); // NOI18N
@@ -95,43 +119,7 @@ public class MainFrame extends javax.swing.JFrame {
                 But_InventoryActionPerformed(evt);
             }
         });
-        jPanel1.add(But_Inventory, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 140, 150, 60));
-
-        System.out.println("Table data loading...");
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
-            new Object[][] {
-                {new ImageIcon(getClass().getResource("C:\\Users\\myaze\\Documents\\NetBeansProjects\\hackpsusp25\\src\\main\\java\\mj\\hackpsusp25\\apple.png")), "Item 1", "Value 1", "Value 2"},
-                {new ImageIcon(getClass().getResource("C:\\Users\\myaze\\Documents\\NetBeansProjects\\hackpsusp25\\src\\main\\java\\mj\\hackpsusp25\\apple.png")), "Item 2", "Value 3", "Value 4"},
-                {null, "Item 3", "Value 5", "Value 6"},
-                {null, "Item 4", "Value 7", "Value 8"}
-            },
-            new String [] {
-                "Image", "Title 2", "Title 3", "Title 4"
-            }
-        ));
-        for (int i = 0; i < jTable1.getRowCount(); i++) {
-            for (int j = 0; j < jTable1.getColumnCount(); j++) {
-                System.out.println("Row " + i + ", Col " + j + ": " + jTable1.getValueAt(i, j));
-            }
-        }
-        System.out.println("Finished loading table data.");
-        jScrollPane1.setViewportView(jTable1);
-
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel2Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 870, Short.MAX_VALUE)
-                .addGap(24, 24, 24))
-        );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 340, Short.MAX_VALUE)
-        );
-
-        jPanel1.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 220, 900, 340));
+        Main_Pannel.add(But_Inventory, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 140, 150, 60));
 
         buttonGroup1.add(But_Add);
         But_Add.setFont(new java.awt.Font("SansSerif", 1, 18)); // NOI18N
@@ -142,7 +130,7 @@ public class MainFrame extends javax.swing.JFrame {
                 But_AddActionPerformed(evt);
             }
         });
-        jPanel1.add(But_Add, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 140, 150, 60));
+        Main_Pannel.add(But_Add, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 140, 150, 60));
 
         buttonGroup1.add(But_Sub);
         But_Sub.setFont(new java.awt.Font("SansSerif", 1, 18)); // NOI18N
@@ -153,39 +141,202 @@ public class MainFrame extends javax.swing.JFrame {
                 But_SubActionPerformed(evt);
             }
         });
-        jPanel1.add(But_Sub, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 140, 150, 60));
+        Main_Pannel.add(But_Sub, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 140, 150, 60));
+
+        Base_Pannel.setLayout(new java.awt.CardLayout());
+
+        jLabel3.setFont(new java.awt.Font("SansSerif", 1, 14)); // NOI18N
+        jLabel3.setText("Search: ");
+
+        jTextField1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField1ActionPerformed(evt);
+            }
+        });
+
+        jLabel4.setFont(new java.awt.Font("SansSerif", 1, 14)); // NOI18N
+        jLabel4.setText("Filters: ");
+
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Image", "Name", "Quantity", "Filters"
+            }
+        ));
+        jScrollPane1.setViewportView(jTable1);
+
+        javax.swing.GroupLayout Inventory_PannelLayout = new javax.swing.GroupLayout(Inventory_Pannel);
+        Inventory_Pannel.setLayout(Inventory_PannelLayout);
+        Inventory_PannelLayout.setHorizontalGroup(
+            Inventory_PannelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(Inventory_PannelLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 84, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 167, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(94, 94, 94)
+                .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 57, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(Inventory_PannelLayout.createSequentialGroup()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 869, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 1241, Short.MAX_VALUE))
+        );
+        Inventory_PannelLayout.setVerticalGroup(
+            Inventory_PannelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(Inventory_PannelLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(Inventory_PannelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.CENTER)
+                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel3)
+                    .addComponent(jLabel4)
+                    .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(1857, Short.MAX_VALUE))
+        );
+
+        Base_Pannel.add(Inventory_Pannel, "card2");
+
+        jLabel5.setFont(new java.awt.Font("SansSerif", 1, 14)); // NOI18N
+        jLabel5.setText("Barcode: ");
+
+        jTextField2.setFont(new java.awt.Font("SansSerif", 1, 14)); // NOI18N
+        jTextField2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField2ActionPerformed(evt);
+            }
+        });
+
+        jTable2.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Image", "Name", "Quantity", "Filters"
+            }
+        ));
+        jScrollPane2.setViewportView(jTable2);
+
+        javax.swing.GroupLayout Add_PannelLayout = new javax.swing.GroupLayout(Add_Pannel);
+        Add_Pannel.setLayout(Add_PannelLayout);
+        Add_PannelLayout.setHorizontalGroup(
+            Add_PannelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(Add_PannelLayout.createSequentialGroup()
+                .addGap(18, 18, 18)
+                .addComponent(jLabel5)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 189, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(Add_PannelLayout.createSequentialGroup()
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 884, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 1226, Short.MAX_VALUE))
+        );
+        Add_PannelLayout.setVerticalGroup(
+            Add_PannelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(Add_PannelLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(Add_PannelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel5)
+                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(26, 26, 26)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 448, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(1825, Short.MAX_VALUE))
+        );
+
+        Base_Pannel.add(Add_Pannel, "card3");
+
+        javax.swing.GroupLayout Sub_PannelLayout = new javax.swing.GroupLayout(Sub_Pannel);
+        Sub_Pannel.setLayout(Sub_PannelLayout);
+        Sub_PannelLayout.setHorizontalGroup(
+            Sub_PannelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 2110, Short.MAX_VALUE)
+        );
+        Sub_PannelLayout.setVerticalGroup(
+            Sub_PannelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 2330, Short.MAX_VALUE)
+        );
+
+        Base_Pannel.add(Sub_Pannel, "card4");
+
+        javax.swing.GroupLayout Org_PannelLayout = new javax.swing.GroupLayout(Org_Pannel);
+        Org_Pannel.setLayout(Org_PannelLayout);
+        Org_PannelLayout.setHorizontalGroup(
+            Org_PannelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 2110, Short.MAX_VALUE)
+        );
+        Org_PannelLayout.setVerticalGroup(
+            Org_PannelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 2330, Short.MAX_VALUE)
+        );
+
+        Base_Pannel.add(Org_Pannel, "card5");
+
+        Main_Pannel.add(Base_Pannel, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 220, 2110, 2330));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 1230, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(Main_Pannel, javax.swing.GroupLayout.PREFERRED_SIZE, 1230, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(0, 0, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(Main_Pannel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void But_OrgActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_But_OrgActionPerformed
-        // TODO add your handling code here:
+        Add_Pannel.setVisible(false);
+        Sub_Pannel.setVisible(false);
+        Org_Pannel.setVisible(true);
+        Inventory_Pannel.setVisible(false);
     }//GEN-LAST:event_But_OrgActionPerformed
 
     private void But_InventoryActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_But_InventoryActionPerformed
-        // TODO add your handling code here:
+        Add_Pannel.setVisible(false);
+        Sub_Pannel.setVisible(false);
+        Org_Pannel.setVisible(false);
+        Inventory_Pannel.setVisible(true);
+        
     }//GEN-LAST:event_But_InventoryActionPerformed
 
     private void But_AddActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_But_AddActionPerformed
-        // TODO add your handling code here:
+        Add_Pannel.setVisible(true);
+        Sub_Pannel.setVisible(false);
+        Org_Pannel.setVisible(false);
+        Inventory_Pannel.setVisible(false);
     }//GEN-LAST:event_But_AddActionPerformed
 
     private void But_SubActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_But_SubActionPerformed
-        // TODO add your handling code here:
+        Add_Pannel.setVisible(false);
+        Sub_Pannel.setVisible(true);
+        Org_Pannel.setVisible(false);
+        Inventory_Pannel.setVisible(false);
     }//GEN-LAST:event_But_SubActionPerformed
+
+    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextField1ActionPerformed
+
+    private void jTextField2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextField2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -225,17 +376,32 @@ public class MainFrame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel Add_Pannel;
+    private javax.swing.JPanel Base_Pannel;
     private javax.swing.JToggleButton But_Add;
     private javax.swing.JToggleButton But_Inventory;
     private javax.swing.JToggleButton But_Org;
     private javax.swing.JToggleButton But_Sub;
+    private javax.swing.JPanel Inventory_Pannel;
+    private javax.swing.JPanel Main_Pannel;
+    private javax.swing.JPanel Org_Pannel;
+    private javax.swing.JPanel Sub_Pannel;
     private javax.swing.ButtonGroup buttonGroup1;
     private javax.swing.ButtonGroup buttonGroup2;
+    private javax.swing.JCheckBoxMenuItem jCheckBoxMenuItem1;
+    private javax.swing.JCheckBoxMenuItem jCheckBoxMenuItem2;
+    private javax.swing.JComboBox<String> jComboBox1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JMenu jMenu1;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JTable jTable1;
+    private javax.swing.JTable jTable2;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jTextField2;
     // End of variables declaration//GEN-END:variables
 }
