e4771227d643369c88e83568ade66282967d7ac4
Jenicy Strong <jlatryce12@gmail.com>
2025-03-29 14:12:17 -0400

    first commit

---
+++ src/main/java/mj/hackpsusp25/Hackpsusp25.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mj.hackpsusp25;
+
+/**
+ *
+ * @author icy
+ */
+public class Hackpsusp25 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
