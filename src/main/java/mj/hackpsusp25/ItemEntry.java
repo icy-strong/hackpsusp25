d93af942d8a576fe8f6debdea8f1bb000a26e1b9
icy-strong <jlatryce12@gmail.com>
2025-03-29 15:10:30 -0400

    db connection & item entry

---
+++ src/main/java/mj/hackpsusp25/ItemEntry.java
@@ -0,0 +1,62 @@
+package mj.hackpsusp25;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author icy
+ */
+public class ItemEntry {
+   private int itemId;                // Unique ID for internal reference
+    private String barcode;            // Barcode for scanning
+    private String name;               // Item name
+    private String description;        // Item description
+    private ArrayList<Integer> categoryIds; // List of category IDs (for multi-category support)
+    private int quantity;              // Inventory count
+    private String imageUrl;           // Image URL or path
+    
+    
+    
+    public ItemEntry(String barcode, String name, String description, 
+                     int supplierId, int quantity, double price, String imageUrl) {
+        this.barcode = barcode;
+        this.name = name;
+        this.description = description;
+        this.quantity = quantity;
+        this.imageUrl = imageUrl;
+        this.categoryIds = new ArrayList<>();
+    }
+    
+    
+    public void addCategory(int categoryID){
+        
+        if(!categoryIds.contains(Integer.valueOf(categoryID))){
+            categoryIds.add(Integer.valueOf(categoryID));
+        }
+    }
+    
+    public void removeCategory(int categoryID){
+        categoryIds.remove(Integer.valueOf(categoryID));
+    }
+
+    // Getters
+    public int getItemId() { return itemId; }
+    public String getBarcode() { return barcode; }
+    public String getName() { return name; }
+    public String getDescription() { return description; }
+    public ArrayList<Integer> getCategoryIds() { return categoryIds; }
+    public int getQuantity() { return quantity; }
+    public String getImageUrl() { return imageUrl; }
+
+    // Setters
+    public void setItemId(int itemId) { this.itemId = itemId; }
+    public void setBarcode(String barcode) { this.barcode = barcode; }
+    public void setName(String name) { this.name = name; }
+    public void setDescription(String description) { this.description = description; }
+    public void setCategoryIds(ArrayList<Integer> categoryIds) { this.categoryIds = categoryIds; }
+    public void setQuantity(int quantity) { this.quantity = quantity; }
+    public void setImageUrl(String imageUrl) { this.imageUrl = imageUrl; }
+
+}
+
+
